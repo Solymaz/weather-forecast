516e7bb88ddd4cecd724da44012f63b1894c7918
Solymaz <44635076+Solymaz@users.noreply.github.com>
2020-12-15 23:48:59 +0100

    fetch weather data from live location

--- src/Search.js
+++ src/Search.js
@@ -13,12 +13,27 @@ export default function Search(props) {
       temperature: Math.round(response.data.main.temp),
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      wind: response.data.wind.speed,
+      wind: Math.round(response.data.wind.speed),
       city: response.data.name,
       min: Math.round(response.data.main.temp_min),
       max: Math.round(response.data.main.temp_max),
       date: response.data.dt * 1000,
       feelsLike: Math.round(response.data.main.feels_like),
+    });
+  }
+
+  function handleLocation(response) {
+    console.log(response);
+    props.setWeatherData({
+      temperature: Math.round(response.data.list[0].main.temp),
+      humidity: response.data.list[0].main.humidity,
+      description: response.data.list[0].weather[0].description,
+      wind: Math.round(response.data.list[0].wind.speed),
+      city: response.data.city.name,
+      min: Math.round(response.data.list[0].main.temp_min),
+      max: Math.round(response.data.list[0].main.temp_max),
+      date: response.data.list[0].dt * 1000,
+      feelsLike: Math.round(response.data.list[0].main.feels_like),
     });
   }
 
@@ -36,9 +51,26 @@ export default function Search(props) {
     event.target.children[0].blur();
     event.target.reset();
   }
+
   function changeCity(event) {
     setCity(event.target.value);
   }
+
+  function getCoords(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(findLiveLocation);
+  }
+
+  function findLiveLocation(GPS) {
+    const lon = GPS.coords.longitude;
+    const lat = GPS.coords.latitude;
+    let locationApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=f2ba4b7c95e0f3e8dedeafe2da9d569f&units=metric`;
+    axios
+      .get(locationApiUrl)
+      .then(handleLocation)
+      .catch(() => {});
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <input
@@ -47,6 +79,9 @@ export default function Search(props) {
         placeholder="Type a city.."
         onChange={changeCity}
       />
+      <span onClick={getCoords} className="pin">
+        📍
+      </span>
     </form>
   );
 }
